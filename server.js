533c8f12d3eefb05d13035b699805ca07c0d6522
Yoeri Otten <yoeori@live.nl>
2016-02-05 21:00:22 +0100

    added support for suburls

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ var bodyParser = require('body-parser');
 var server = express();
 
 var App = require('./app');
+
+console.log(process.env);
+
 var app = new App(require("./secrets"), require("./data"), function() {
-  
-  server.use(bodyParser.json());
-  server.use(bodyParser.urlencoded({extended: true}));
+
+  var router = express.Router();
+  router.use(bodyParser.json());
+  router.use(bodyParser.urlencoded({extended: true}));
 
   //serve index page etc.
-  server.use(express.static('public'));
+  router.use(express.static('public'));
 
   //download the iCal file
-  server.get('/:school/:username/:password', function (req, res) {
+  router.get('/:school/:username/:password', function (req, res) {
     app.getCalendarFromData({school: req.params.school, username: req.params.username, password: req.params.password}, function(ical) {
       if(ical != false) ical.serve(res);
       else res.end('404');
@@ -20,9 +24,11 @@ var app = new App(require("./secrets"), require("./data"), function() {
   });
 
   //generate encrypted password from supplied args
-  server.post('/generate', function(req, res) {
+  router.post('/generate', function(req, res) {
     res.end(app.encryptText(req.body.password));
   });
+
+  server.use(process.env.NODE_SUBURL || '', router);
 
   var listener = server.listen(3000, function() {
     console.log('The magister calendar service has started on http://%s:%s.', listener.address().address, listener.address().port);
